52e3dd63f25e084756806e6d970dffe2b56793d9
yash-raj202134 <yashraj3376@gmail.com>
2025-05-14 00:07:11 +0530

    added validations to inputs

--- remind_me_later/reminders/serializers.py
+++ remind_me_later/reminders/serializers.py
@@ -1,3 +1,4 @@
+from datetime import timezone
 from rest_framework import serializers
 from .models import Reminder
 
@@ -5,3 +6,13 @@ class ReminderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reminder
         fields = '__all__'
+
+    def validate_date(self, value):
+        if value < timezone.now().date():
+            raise serializers.ValidationError("The date must be in the future.")
+        return value
+
+    def validate_time(self, value):
+        if value < timezone.now().time():
+            raise serializers.ValidationError("The time must be in the future.")
+        return value
