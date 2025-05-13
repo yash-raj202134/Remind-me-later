4edaf770725d389b8d2c150a8850c052b1bd8701
yash-raj202134 <yashraj3376@gmail.com>
2025-05-13 22:17:26 +0530

    remainder model created

--- remind_me_later/reminders/models.py
+++ remind_me_later/reminders/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Reminder(models.Model):
+    REMINDER_CHOICES = [
+        ('SMS', 'SMS'),
+        ('Email', 'Email'),
+    ]
+    
+    date = models.DateField()
+    time = models.TimeField()
+    message = models.TextField()
+    reminder_type = models.CharField(max_length=10, choices=REMINDER_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.message} @ {self.date} {self.time} via {self.reminder_type}"
